f0e9f6ad397cf85970c53652878399c28b4ce2a5
Antoni Wiguna <antoni.wiguna@gmail.com>
2017-09-06 10:08:03 +0700

    Initial Commit

---
+++ TestLib/TestLib.h
@@ -0,0 +1,19 @@
+//
+//  TestLib.h
+//  TestLib
+//
+//  Created by Antoni Wiguna on 9/6/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestLib.
+FOUNDATION_EXPORT double TestLibVersionNumber;
+
+//! Project version string for TestLib.
+FOUNDATION_EXPORT const unsigned char TestLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLib/PublicHeader.h>
+
+
